95f3a0de3e4d3f7b1da93199abe3cd0cfd2c6e4f
Adam Cox (Vorba Corp) <adam.cox@vorba.com>
2015-02-10 00:26:12 -0500

    Add registration & authorization

---
+++ Purevision/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Purevision.Startup))]
+namespace Purevision
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
