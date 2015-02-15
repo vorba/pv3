b84445274d8bfac5aa8b171a428f4121cdc69d52
Adam Cox (Vorba Corp) <adam.cox@vorba.com>
2015-02-15 18:02:30 -0500

    UpdatePhoneEmailLength

---
+++ Purevision/Migrations/201502152259196_UpdatePhoneEmailLength.cs
@@ -0,0 +1,18 @@
+namespace Purevision.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdatePhoneEmailLength : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.People", "Phone", c => c.String(nullable: false, maxLength: 14));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.People", "Phone", c => c.String(nullable: false, maxLength: 10));
+        }
+    }
+}
